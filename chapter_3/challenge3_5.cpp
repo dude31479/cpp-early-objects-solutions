0c817f25b6a694faffcdb06c3ee23acb52f6843d
Chris Evans <c.evans1793@proton.me>
2025-04-10 20:07:52 -0400

    Refactor

--- chapter_3/challenge3_5.cpp
+++ chapter_3/challenge3_5.cpp
@@ -1,19 +1,36 @@
-// Batting average
+/*************************************
+ *                                   *
+ *          Batting Average          *
+ *                                   *
+ *  This is a simple program that    *
+ *  calculates a batting average by  *
+ *  typecasting an int to a double.  *
+ *                                   *
+ ************************************/
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+
 int main()
 {
-    double average;
-    int times, hits;
+    
+    double batting_average;
+    int    hits, at_bat;
+
+    cout << "This program will calculate a player's\n"
+         << "batting average for a single game.\n\n";
+
     cout << "How many times was the player at bat? ";
-    cin >> times;
-    cout << "How many hits did he get? ";
-    cin >> hits;
-    average = static_cast<double>(hits) / times;
+    cin  >> at_bat;
+    cout << "How many hits did the player get? ";
+    cin  >> hits;
+
+    batting_average = static_cast<double>(hits) / at_bat;
 
     cout << fixed << setprecision(4);
-    cout << "Batting average: " << average << endl;
+    cout << "Out of " << at_bat << " times at bat,\n"
+         << "the player's batting average was " 
+         << batting_average << ".\n";
     return 0;
 }
