301d1e6a4f3fe89630a98e86570c7259aadda133
Chris Evans <c.evans1793@proton.me>
2025-04-10 20:36:57 -0400

    Full refactor

--- chapter_3/challenge3_7.cpp
+++ chapter_3/challenge3_7.cpp
@@ -1,5 +1,8 @@
-// Average Rainfall
-// This program calculates the three month average rainfall
+/****************************
+ *                          *
+ *      Average Rainfall    *
+ *                          *
+ * *************************/
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -7,31 +10,37 @@ using namespace std;
 
 int main()
 {
-    string month1, month2, month3;
-    double rain1, rain2, rain3, total, avg;
+    string month;
 
-    cout << "This program calculates the average rainfall of three consecutive months.\n";
-    cout << "Enter month 1: ";
-    getline(cin, month1);
-    cout << "What was the amount of rain in " << month1 << "? " << endl;
-    cin >> rain1;
-    cin.get();
-    cout << "Enter month 2: ";
-    getline(cin, month2);
-    cout << "What was the amount of rain in " << month2 << "? " << endl;
-    cin >> rain2;
-    cin.get();
-    cout << "Enter month 3: ";
-    getline(cin, month3);
-    cout <<"And how much rain was there in " << month3 << "? " << endl;
-    cin >> rain3;
-    cin.get();
+    double rain, 
+           total = 0, 
+           avg = 0;
 
-    total = rain1 + rain2 + rain3;
+    cout << "\nThis program calculates the average rainfall of three consecutive months.\n";
+    
+    string output = "The average monthly rainfall for ";
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter month " << (i+1) << ": ";
+        cin  >> month;
+        cout << "What was the amount of rain in " << month << "? ";
+        cin  >> rain;
+        cin.get();
+        total += rain;
+        if (i < 2) 
+        {
+            output += month + ", ";
+        }
+        else 
+        {
+            output += "and " + month;
+        }
+    }
+
     avg = total / 3;
 
-    cout << "The average monthly rainfall for " << month1
-         << ", " << month2 << ", and " << month3;
+    cout << output;
     cout << " was " << fixed << setprecision(2) << avg << " inches.\n";
     return 0;
 }
